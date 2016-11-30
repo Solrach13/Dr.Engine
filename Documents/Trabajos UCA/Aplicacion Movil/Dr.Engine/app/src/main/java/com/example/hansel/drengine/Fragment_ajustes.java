dc020849292338ae4ad6370ab4a819ccde18592b
Francella Zeledon <Francella>
2016-11-30 17:48:40 -0600

    Pantallas Francella

---
+++ Documents/Trabajos UCA/Aplicacion Movil/Dr.Engine/app/src/main/java/com/example/hansel/drengine/Fragment_ajustes.java
@@ -0,0 +1,69 @@
+package com.example.hansel.drengine;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Zeledon on 29/11/2016.
+ */
+
+public class Fragment_ajustes extends Fragment{
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private Fragment_3.OnFragmentInteractionListener mListener;
+
+    public Fragment_ajustes() {
+        //constructor
+    }
+
+    public static Fragment_ajustes newInstance(String param1, String param2) {
+        Fragment_ajustes fragment = new Fragment_ajustes();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_2, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
+
